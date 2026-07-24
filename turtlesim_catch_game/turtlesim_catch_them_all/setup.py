6781ccdb635c8d018d92b0f37856bf36b7f6fea1
Xiaohang Liu <1762934741@qq.com>
2026-07-24 00:28:57 -0400

    Add interfaces

---
+++ turtlesim_catch_game/turtlesim_catch_them_all/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtlesim_catch_them_all'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yuto',
+    maintainer_email='1762934741@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "turtle_controller = turtlesim_catch_them_all.turtle_controller:main",
+            "turtle_spawner = turtlesim_catch_them_all.turtle_spawner:main"
+        ],
+    },
+)
---
+++ turtlesim_catch_game/turtlesim_catch_them_all/turtlesim_catch_them_all/turtle_controller.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+import math
+import rclpy
+from rclpy.node import Node
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
+from functools import partial
+from my_robot_interfaces.msg import Turtle, TurtleArray
+from my_robot_interfaces.srv import CatchTurtle
+
+# Given a coordinate point(x, y) then control turtle move to the destination
+class TurtleControllerNode(Node):
+    
+    def __init__(self):
+        super().__init__("turtle_controller")
+        self.declare_parameter("catch_closest_turtle_first", True)
+
+        self.cur_pose_: Pose = None
+        self.turtle_to_catch_: Turtle = None
+        self.catch_closest_turtle_first_ = self.get_parameter(
+            "catch_closest_turtle_first").value
+        self.turtle_cmdVel_publisher_ = self.create_publisher(
+            Twist, "/turtle1/cmd_vel", 10)
+        self.create_subscription(
+            Pose, "/turtle1/pose", self.callback_turtle_pose, 10)  #run the callback as fast as we receive the message
+        self.alive_turtles_subscriber_ = self.create_subscription(
+            TurtleArray, "alive_turtles", self.callback_alive_turtles, 10)
+        self.create_timer(0.01, self.control_loop)
+
+        self.catch_turtle_client_ = self.create_client(CatchTurtle, "catch_turtle")
+
+    # Save the current pose
+    def callback_turtle_pose(self, pose: Pose):
+        self.cur_pose_ = pose
+
+    def callback_alive_turtles(self, msg: TurtleArray):
+        if len(msg.turtles) > 0:
+            if self.catch_closest_turtle_first_:
+                closest_turtle = None
+                closest_turtle_distance = None
+
+                for turtle in msg.turtles:
+                    dis_x = turtle.x - self.cur_pose_.x
+                    dis_y = turtle.y - self.cur_pose_.y
+                    dis = math.sqrt(dis_x**2 + dis_y**2)
+                    if closest_turtle == None or dis < closest_turtle_distance:
+                        closest_turtle = turtle
+                        closest_turtle_distance = dis
+                self.turtle_to_catch_ = closest_turtle
+            else:    
+                self.turtle_to_catch_ = msg.turtles[0]
+
+    # calculate the distance and orientation and then pushlish cmd_vel topic
+    def control_loop(self):    
+        if self.cur_pose_ == None or self.turtle_to_catch_ == None:
+            return 
+        
+        dis_x = self.turtle_to_catch_.x - self.cur_pose_.x
+        dis_y = self.turtle_to_catch_.y - self.cur_pose_.y
+        dis = math.sqrt(dis_x**2 + dis_y**2)
+
+        x_angular = Twist()
+
+        if dis > 0.5:
+            # position
+            x_angular.linear.x = dis
+            # orientation
+            goal_theta = math.atan2(dis_y, dis_x)
+            diff = goal_theta - self.cur_pose_.theta
+            # Need to within [-180,180] degree
+            if diff > math.pi:
+                diff -= 2*math.pi
+            elif diff < -math.pi:
+                diff += 2*math.pi
+            x_angular.angular.z = diff
+        else:
+            # target reached 
+            x_angular.linear.x = 0.0
+            x_angular.angular.z = 0.0    
+
+            self.call_catch_turtle_service(self.turtle_to_catch_.name)
+            self.turtle_to_catch_ = None
+        
+        self.turtle_cmdVel_publisher_.publish(x_angular)
+
+    def call_catch_turtle_service(self, turtle_name):
+        while not self.catch_turtle_client_.wait_for_service(1.0):
+            self.get_logger().info("waiting for catch turtle service...")
+        
+        request = CatchTurtle.Request()
+        request.name = turtle_name
+
+        future = self.catch_turtle_client_.call_async(request)
+        future.add_done_callback(
+            partial(self.callback_catch_turtle_service, turtle_name = turtle_name)
+        )
+    
+    def callback_catch_turtle_service(self, future, turtle_name):
+        reponse: CatchTurtle.Response = future.result()
+        if not reponse.success:
+            self.get_logger().error("turtle " + turtle_name + " could not be removed")
+
+
+    
+def main(args = None):
+    rclpy.init(args=args)
+    node = TurtleControllerNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
