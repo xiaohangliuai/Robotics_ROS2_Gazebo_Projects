dfb19dbecd13df94993e09568bf6660c6719ef3b
Xiaohang Liu <1762934741@qq.com>
2026-07-31 23:09:31 -0400

    Add action server

---
+++ ros2_study_ws/src/actions_py/actions_py/count_until_server.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+import time
+from rclpy.action import ActionServer
+from rclpy.action.server import ServerGoalHandle
+from my_robot_interfaces.action import CountUntill
+
+class CountUntilActionServer(Node):
+    def __init__(self):
+        super().__init__('count_until_action_server')
+        self._action_server = ActionServer(
+            self,
+            CountUntill,
+            'count_until',
+            self.execute_callback
+        )
+        self.get_logger().info('CountUntil Action Server has been started.')
+
+    def execute_callback(self, goal_handle: ServerGoalHandle):
+        # Implementation for handling the action execution goes here
+        # get request from goal
+        target_number = goal_handle.request.target_number
+        period = goal_handle.request.period
+        self.get_logger().info(f'Request received for counting until {target_number} with period {period}.')
+
+        # Excecute the action counting logic
+        self.get_logger().info('Executing count until action.')
+        counter = 0
+        for i in range(target_number):
+            counter += 1
+            self.get_logger().info(f'Current count: {counter}')
+            time.sleep(period)
+
+        # Once the counting is done, set the result and mark the goal as succeeded
+        goal_handle.succeed()
+        result = CountUntill.Result()
+        result.reached_number = counter
+        self.get_logger().info(f'Counting completed. Final count: {result.reached_number}')
+        return result
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    action_server = CountUntilActionServer()
+    rclpy.spin(action_server)
+    action_server.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ ros2_study_ws/src/actions_py/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'actions_py'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yuto',
+    maintainer_email='1762934741@qq.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            "count_until_server = actions_py.count_until_server:main"
+        ],
+    },
+)
