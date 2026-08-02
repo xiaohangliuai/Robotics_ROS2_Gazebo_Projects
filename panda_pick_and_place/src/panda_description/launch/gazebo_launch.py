8255123d9c03b8aba400f1c4c6f47b8d44b7c4bf
Xiaohang Liu <1762934741@qq.com>
2026-08-02 17:42:04 -0400

    Setup the initial env in gazebo

---
+++ panda_pick_and_place/src/panda_description/launch/gazebo_launch.py
@@ -0,0 +1,111 @@
+import os
+from os import pathsep
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    panda_description = get_package_share_directory("panda_description")
+
+    model_arg = DeclareLaunchArgument(
+        name="model", default_value=os.path.join(
+                panda_description, "urdf", "panda_urdf.xacro"
+            ),
+        description="Absolute path to robot urdf file"
+    )
+
+    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="empty")
+
+    world_path = PathJoinSubstitution([
+            panda_description,
+            "worlds",
+            PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"])
+        ]
+    )
+
+    model_path = str(Path(panda_description).parent.resolve())
+    model_path += pathsep + os.path.join(get_package_share_directory("panda_description"), 'models')
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH",
+        model_path
+        )
+
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_ignition = "True" if ros_distro == "Jazzy" else "False"
+
+    robot_description = ParameterValue(Command([
+            "xacro ",
+            LaunchConfiguration("model"),
+            " is_ignition:=",
+            is_ignition
+        ]),
+        value_type=str
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
+    )
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+                launch_arguments={
+                    "gz_args": PythonExpression(["'", world_path, " -v 4 -r'"])
+                }.items()
+             )
+
+
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-topic", "robot_description",
+            "-name", "panda",
+            "-x", "0.0",  
+            "-y", "0.0",  
+            "-z", "0.0",  
+            "-R", "0.0", 
+            "-P", "0.0",
+            "-Y", "0.0", # Yaw (in radians, e.g., 1.57 for 90 degrees)
+        ],
+    )
+
+
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/camera/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo"
+        ],
+    )
+
+    ros_gz_image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/image_raw"]
+    )
+
+    return LaunchDescription([
+        model_arg,
+        world_name_arg,
+        gazebo_resource_path,
+        robot_state_publisher_node,
+        gazebo,
+        gz_spawn_entity,
+        # gz_ros2_bridge,
+        # ros_gz_image_bridge
+    ])
